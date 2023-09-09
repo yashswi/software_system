9c844a630b7dfc0d8184a6153584332d610d43c0
yashswi <90143146+yashswi@users.noreply.github.com>
2023-09-09 18:18:32 +0530

    Added 27d.c

---
+++ 27d.c
@@ -0,0 +1,24 @@
+/*
+============================================================================
+Name : 27b.c
+Author : yashswi Chaturvedi
+Description : Write a program to execute ls -Rl by the following system calls
+a. execl
+b. execlp
+c. execled. execv
+e. execvp
+Date: 5th sep, 2023.
+============================================================================
+*/
+#include <unistd.h> 
+#include <stdio.h>  
+
+
+int main()
+{
+    char *arguments[]={"ls", "-Rl", NULL}; 
+    execv("/bin/ls", arguments);
+    printf("\n This printf statement will execute if execv fails!!!");
+
+    return 0;
+}
