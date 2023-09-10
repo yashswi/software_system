6a9519c16784a2d4257fec5fa532af90694219ea
yashswi <90143146+yashswi@users.noreply.github.com>
2023-09-10 09:58:47 +0530

    Added 2.c

---
+++ 2.c
@@ -0,0 +1,22 @@
+/*
+============================================================================
+Name : 2.c
+Author : yashswi Chaturvedi
+Description :Write a simple program to execute in an infinite loop at the background. Go to /proc directory and
+identify all the process related information in the corresponding proc directory.
+
+Date: 1 Aug, 2023.
+============================================================================
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main() {
+
+   for(;;);
+    return 0;
+}
